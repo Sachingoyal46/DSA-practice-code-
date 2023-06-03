aae43c0d90d9326a9142bb911bccc6b2c11c44ef
Sachin Goyal <sachingoel8171591468@gmail.com>
2023-06-03 13:21:37 +0530

    code

---
+++ L3.cpp
@@ -0,0 +1,290 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    //    int n;
+    //    cin>>n;
+    //    int t=0;
+
+    //    for(int i=0;i<=n;i++)
+    //    {
+
+       
+    //    if(i%2==0)
+    //    {
+    //     t=t+i;
+    //    }
+      
+
+    //    }
+
+    //    cout<<t;
+
+
+    // int n;
+    // cin>>n;
+    // int i;
+    // int flag=0;
+    // for( i=2;i<n;i++)
+    // {
+    //     if(n%i==0)
+    //     {
+    //        break;
+    //     }
+    //     else
+    //     {
+    //         flag++;
+    //     }
+    // }
+    // if(flag==0)
+    // {
+    //     if(n!=2)
+    //     printf("not prime");
+    //     else
+    //     printf("prime");
+    // }
+    // else
+
+    // {
+    //     cout<<"prime";
+    // }
+
+    // int n;
+    // cin>>n;
+
+    // int i=1;
+
+    // while(i<=n)
+    // {
+    //     int j=1;
+    //     while(j<=n)
+    //     {
+    //         cout<<"*";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    //  int n;
+    // cin>>n;
+
+    // int i=1;
+    // int k=1;
+
+    // while(i<=n)
+    // {
+    //     int j=1;
+    //     while(j<=n)
+    //     {   
+            
+    //         cout<<i;
+    //         j++;
+    //     }
+    //     k++;
+    //     cout<<endl;
+    //     i++;
+    // }
+
+
+
+    //  int n;
+    // cin>>n;
+
+    // int i=1;
+   
+    // while(i<=n)
+    // {
+    //     int j=n;
+    //     while(j>0)
+    //     {   
+            
+    //         cout<<j;
+    //         j--;
+    //     }
+        
+    //     cout<<endl;
+    //     i++;
+    // }
+
+//     int n;
+//     cin>>n;
+//   int count =1;
+
+//     int i=1;
+//     while(i<=n)
+//     {
+//         int j=1;
+//         while(j<=n)
+//         {
+//             cout<<count;
+//             count ++;
+//             j++;
+//         }
+//         j++;
+        
+//         cout<<endl;
+        
+//         i++;
+
+//triangle pattern
+
+//  int n;
+// cin>>n;
+//   int count =1;
+
+//     int i=1;
+//     while(i<=n)
+//     {
+//         int j=1;
+//         while(j<=i)
+//         {
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+
+
+// number triangle *********************************** 
+//  int n;
+// cin>>n;
+//   int count =1;
+
+//     int i=1;
+//     while(i<=n)
+//     {
+//         int j=1;
+//         while(j<=i)
+//         {
+//             cout<<i;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+
+// int  n;
+// cin>>n;
+// int k=1;
+// int i=1;
+
+// while(i<=n)
+// {
+//     int j=1;
+//     int value=i;
+//     while(j<=i)
+//     {
+//         cout<<value;
+//         value++;
+//         j++;
+        
+        
+//     }
+//     cout<<endl;
+//     i++;
+// }
+
+
+
+
+// int  n;
+// cin>>n;
+// int k=1;
+// int i=1;
+
+// while(i<=n)
+// {
+//     int j=i;
+    
+//     while(j>0)
+//     {
+//         cout<<j;
+        
+//         j--;
+//     }
+//     cout<<endl;
+//     i++;
+// }
+
+// }
+
+//alphabets pattern   **********************************
+
+// int n;
+// cin>>n;
+
+// int i=1;
+// while(i<=n)
+// {
+
+//     int j=1;
+//     while(j<=n)
+//     {   
+//         char k=( 65 +i-1);
+//         cout<<k;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+
+
+// }
+// 
+
+
+
+// int n;
+// cin>>n;
+
+// int i=1;
+// while(i<=n)
+// {
+
+//     int j=1;
+//     while(j<=n)
+//     {   
+//         char k=( 65 +j-1);
+//         cout<<k;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+
+
+// }
+
+
+int n;
+cin>>n;
+
+int i=1;
+char c='a';
+while(i<=n)
+{
+
+    int j=1;
+    // char k=( 65 +i-1);
+    while(j<=n)
+    {   
+        
+        cout<<c;
+        j++;
+        c++;
+    }
+    cout<<endl;
+    i++;
+
+
+}
+
+
+
+
+
+
+}
+
